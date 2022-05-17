a939da7dfc2fe44c5edec0248fe4760138ec2c41
justin <yajustin168@gmail.com>
2022-05-17 11:55:24 -0700

    assigned funtion and added error message

--- script.js
+++ script.js
@@ -17,7 +17,15 @@ function generatePassword() {
   var numbers = "1234567890"
   var plength = parseInt(prompt("Enter password length"))
   if(plength < 8 || plength > 126 || isNaN(plength)){
-    return "404 error"
+    return "404 error - Please enter valid length beterrn 8 and 126"
+  }
+  var validCharacters =""
+  var validUpper = confirm("Uppercase ?")
+  var validlower = confirm("Lowercase ?")
+  var validspecial = confirm("special ?")
+  var validnumbers = confirm("number ?")
+  if(!validUpper && !validlower && !validnumbers && !validspecial) {
+    return "404 error - Please choose min 1 option"
   }
 }
 
