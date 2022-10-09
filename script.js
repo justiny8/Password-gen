6f719422655f2d47b14b77b37210b883bef1840c
justiny8 <104675479+justiny8@users.noreply.github.com>
2022-10-09 07:08:03 -0700

    Update script.js
    updated on character length

--- script.js
+++ script.js
@@ -1,33 +1,82 @@
-// Assignment Code
+// Assignment code here
 var generateBtn = document.querySelector("#generate");
+
+//Global character options
+var upperLetters = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+var lowerLetters = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+var specialChars = ('!','@','#','$','%','^','&','*','(',')','<','>','?');
+var numbers = ('1','2','3','4','5','6','7','8','9');
+
+//generate random password based upon criterias via series of prompts
+function generatePassword() {
+  var password = "";
+
+//Password Length
+  var confirmLength = prompt("Choose Character for password");
+  while(confirmLength <= 8 || confirmLength >= 128) {
+    alert("Password length must be between 8-128 characters.");
+    confirmLength = (prompt("How many characters do you want your password to include?"));
+  };
+
+//Variables for selected character types
+  var upperCase = confirm("Click 'OK' to include uppercased characters in your password.")
+  var lowerCase = confirm("Click 'OK' to include lowercased characters in your password.")
+  var specialCase = confirm("Click 'OK' to include special characters in your password.")
+  var numberCase = confirm("Click 'OK' to include numbers in your password.")
+
+//Confirm character types
+  if (upperCase === false && lowerCase === false && specialCase === false && numberCase === false) {
+    window.alert("Please select at least ONE character type");
+  
+  upperCase = window.confirm("Click to inlcude upper cased letters in your password.");
+  lowerCase = window.confirm("Click to include lower cased lettters in your password.");
+  specialCase = window.confirm("Click to include special characters in your password.");
+  numberCase = window.confirm("Click to confirm numbers in your password.");
+  };
+
+
+//Running criteria
+
+  var randomLetter;
+  var chars = [];
+
+  if (upperCase) {
+    chars = chars.concat(upperLetters);
+  }
+  if (lowerCase) {
+    chars = chars.concat(lowerLetters);
+  }
+  if (specialCase) {
+    chars = chars.concat(specialChars);
+  }
+  if (numberCase) {
+    chars = chars.concat(numbers);
+  }
+
+  //randomizing letters
+  for (var i = 0; i <= confirmLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    randomLetter = chars[randomNumber];
+    // password += chars.substring(randomNumber, randomNumber +1);
+
+    password = password + randomLetter;
+  }
+  return password;
+};
+
+// document.getElementById("password").value = password;
+
+// Get references to the #generate element
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var newPassword = generatePassword();
+var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+passwordText.value = newPassword;
 
-}
-
-function generatePassword() {
-  var upper = "ABCDEFGHIJKLMNOPQRST";
-  var lower = "abcdefghijklmnopqrst"
-  var special = "!@#$%^&*()_+"
-  var numbers = "1234567890"
-  var plength = parseInt(prompt("Enter password length"))
-  if(plength < 8 || plength > 126 || isNaN(plength)){
-    return "404 error - Please enter valid length beterrn 8 and 126"
-  }
-  var validCharacters =""
-  var validUpper = confirm("Uppercase ?")
-  var validlower = confirm("Lowercase ?")
-  var validspecial = confirm("special ?")
-  var validnumbers = confirm("number ?")
-  if(!validUpper && !validlower && !validnumbers && !validspecial) {
-    return "404 error - Please choose min 1 option"
-  }
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
